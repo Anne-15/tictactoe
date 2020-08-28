c52b5d56008735ed6615853a2af160ee7e9d81ac
Anne-15 <wariiyuanne@gmail.com>
2020-08-28 17:49:19 +0300

    create board game and positions

--- main.py
+++ main.py
@@ -8,3 +8,25 @@ def display_board():
     print(board[6] + " | " + board[7] + " | " + board[8])
 
 display_board()
+
+def play_game():
+
+#display the board
+    display_board()
+    while game_still_going:
+        handle_turn(current_player)
+        check_if_game_over()
+        flip_player()
+
+
+def handle_turn():
+    position = input("Choose a number from 1-9: ")
+    position = int(position) - 1
+
+    board[position] = 'X'
+    display_board()
+
+    handle_turn()
+play_game()
+
+
