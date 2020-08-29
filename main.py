557d584588b5671b8122a735de29dcfd3930cf5a
Anne-15 <wariiyuanne@gmail.com>
2020-08-29 10:48:23 +0300

    developing the logic functions of the game

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+# ------ global variables ------
+
+#  board game
 board = ["_","_","_",
          "_","_","_",
          "_","_","_"]
+
+#  if game is still going
+
+game_still_on = True
+
+# winner
+winner = None
+current_player = "X"
 
 def display_board():
     print(board[0] + " | " + board[1] + " | " + board[2])
@@ -9,24 +20,42 @@ def display_board():
 
 display_board()
 
+def handle_turn(player):
+        position = input("Choose a number from 1-9: ")
+        position = int(position) - 1
+
+        board[position] = 'X'
+        display_board()
+
 def play_game():
 
-#display the board
-    display_board()
-    while game_still_going:
+    #display the board
+    # display_board()
+    def check_if_game_over():
+        check_if_win()
+        check_if_tie()
+
+    def check_if_win():
+        # check rows, columns and diagonals
+        return
+    def check_if_tie():
+        return
+    def flip_player():
+        return
+
+    while game_still_on:
         handle_turn(current_player)
         check_if_game_over()
         flip_player()
 
+        if winner == "X" or winner == "O":
+            print(winner + " won.")
+        elif winner == None:
+            print("It is a tie!!")
 
-def handle_turn():
-    position = input("Choose a number from 1-9: ")
-    position = int(position) - 1
 
-    board[position] = 'X'
-    display_board()
 
-    handle_turn()
+
 play_game()
 
 
