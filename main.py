be03d2f8790b299c682365756124f0708a2d9b18
Anne-15 <wariiyuanne@gmail.com>
2020-08-29 11:55:32 +0300

    flip players and determine winner

--- main.py
+++ main.py
@@ -19,25 +19,28 @@ def display_board():
     print(board[3] + " | " + board[4] + " | " + board[5])
     print(board[6] + " | " + board[7] + " | " + board[8])
 
-# display_board()
+display_board()
 
 # handle a single turn per player
 def handle_turn(player):
         position = input("Choose a number from 1-9: ")
         position = int(position) - 1
 
-        board[position] = 'X'
-        # display_board()
+        board[position] = player
+        display_board()
 
 # play game function
 def play_game():
-
-    #display the board
-    display_board()
-    # check if the game is over
-    def check_if_game_over():
-        check_for_winner()
-        check_if_tie()
+    # take turns for players to play
+    def flip_player():
+        global current_player
+        # check if player is x, change to o
+        if current_player == "X":
+            current_player = "O"
+        #check if player is o, change to x
+        elif current_player == "O":
+            current_player = "X"
+        return
 
     # check if there is a winner between x and o
     def check_for_winner():
@@ -60,19 +63,69 @@ def play_game():
         return
 
     def check_rows():
+        global game_still_on
+        # check if all the rows are filled and not empty
+        row_1 = board[0] == board[1] == board[2] != "_"
+        row_2 = board[3] == board[4] == board[5] != "_"
+        row_3 = board[6] == board[7] == board[8] != "_"
+
+        # if any row has a match, flag that there is a winner
+        if row_1 or row_2 or row_3:
+            game_still_on = False
+
+        # return the winner
+        if row_1:
+            return board[0]
+        elif row_2:
+            return board[3]
+        elif row_3:
+            return board[6]
         return
 
     def check_columns():
+        global game_still_on
+        # check if all columns are filled
+        column_1 = board[0] == board[3] == board[6] != "_"
+        column_2 = board[1] == board[4] == board[7] != "_"
+        column_3 = board[2] == board[5] == board[8] != "_"
+
+        # if any of the columns has a match, flag that there is a winner
+        if column_1 or column_2 or column_3:
+            game_still_on = False
+
+        # return winner
+        if column_1:
+            return board[0]
+        elif column_2:
+            return board[1]
+        elif column_3:
+            return board[2]
         return
 
     def check_diagonals():
+        global game_still_on
+        # check if all columns are filled
+        diagonal_1 = board[0] == board[4] == board[8] != "_"
+        diagonal_2 = board[2] == board[4] == board[6] != "_"
+
+        # if any of the columns has a match, flag that there is a winner
+        if diagonal_1 or diagonal_2:
+            game_still_on = False
+
+        # return winner
+        if diagonal_1:
+            return board[0]
+        elif diagonal_2:
+            return board[2]
         return
+
+    # check if the game is over
+    def check_if_game_over():
+        check_for_winner()
+        check_if_tie()
 
     # check whether there is a tie
     def check_if_tie():
-        return
-    # take turns for players to play
-    def flip_player():
         return
 
     # while the game is still goin
@@ -80,11 +133,14 @@ def play_game():
         handle_turn(current_player)
         check_if_game_over()
         flip_player()
+
     # game has ended
     if winner == "X" or winner == "O":
         print(winner + " won.")
     elif winner == None:
         print("It is a tie!!")
+
+
 
 
 play_game()
